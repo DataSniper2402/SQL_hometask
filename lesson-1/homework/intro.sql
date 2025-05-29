c96e797b4222f8ee87056e5002c746822c89971e
DataSniper2402 <m.rozmatov2402@gmail.com>
2025-05-29 16:31:56 +0500

    Create intro.sql

---
+++ lesson-1/homework/intro.sql
@@ -0,0 +1,45 @@
+Easy
+
+    Define the following terms: data, database, relational database, and table.
+  Data deb bizga ma'lum bo'lmagan malumotga aytiladi.
+  Database esa katta tartibli malumotlar bazasiga aytiladi
+  Table=jadval qator va ustundan iborat. 
+
+    List five key features of SQL Server.
+  Database
+  Xavfsiz
+  ma'lumotlarni impoprt - eksport qilish, hisoblar yaratish.
+  ma'lumotlarni kopi grafik shaklida.
+  ma'lumotlar umuman yoqolmaydi
+
+    What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+  Windows authentifikatsiyasi 
+  SQL Server authentifikatsiyasi
+Medium
+
+    Create a new database in SSMS named SchoolDB.
+  SQL buyrug'i orqali 
+    Grafik interfeys orqali
+
+    Write and execute a query to create a table called Students with columns: StudentID (INT, PRIMARY KEY), Name (VARCHAR(50)), Age (INT).
+  CREATE TABLE Students
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+
+    Describe the differences between SQL Server, SSMS, and SQL.
+  SQL server Microsoft tomonidan chiqarilgan ma'lumotlar bazasi 
+  SQL serverga ulanish va u bilan ishlash uchun mo'ljallangan grafik interfeysli dasturi
+  SQL malumotlar bilan ishlash uchun maxsus til 
+Hard
+    Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
+  DQL malumotlarni sorov qilish select buyrugi bilan
+  DML malumotlarni o'zgartirish 
+  DDL ma'lumotlar bazasi strukturasi 
+  DCL foydalanuvchi huquqlarini boshqarish 
+  TCL tranzaksiyalarni boshqarish.
+    Write a query to insert three records into the Students table.
+  'Ali' , 20
+  'Dilnoza' , 19
+  'Sardor' ,  21
+    Restore AdventureWorksDW2022.bak file to your server. (write its steps to submit) You can find the database from this link :https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2022.bak
